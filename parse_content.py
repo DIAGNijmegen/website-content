ef936f56ec5a43aca9695e663bcdd41c26bfdc8f
Wouter Bulten <wouterbulten@mai-ai.org>
2019-01-21 13:33:24 +0100

    Type in content script

--- parse_content.py
+++ parse_content.py
@@ -43,8 +43,8 @@ for dir in directories:
                                 out_path = os.path.join(group_path, 'content', 'pages', dir, filename)
                             print(f"CP {file_path} to {out_path}")
                             if not os.path.exists(out_path):
-                                os.makedirs(directory)
-                        
+                                os.makedirs(out_path)
+
                             shutil.copyfile(file_path, out_path)
                     except Exception as e:
                         print(f"Error parsing {file_path}.")
