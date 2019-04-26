6090b7aee4815025e65795ebb98b293c0040ebcb
Wouter Bulten <wouterbulten@mai-ai.org>
2019-04-26 11:25:33 +0200

    Set build to verbose

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 # #!/usr/bin/env bash
 
 # Break build on error, prevents websites going offline in case of pelican errors
-set -e
+set -ev
 
 # Optimize the images before building the website`
 cd imgoptim
