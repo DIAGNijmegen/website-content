0e78f7a9bc903b9c3ab03c075d61be9a04e00103
Wouter Bulten <wouterbulten@mai-ai.org>
2019-08-06 17:55:31 +0200

    Update npm install

--- build.sh
+++ build.sh
@@ -20,7 +20,7 @@ else
 
   # Add changed files
   git checkout master
-  git reset -- package-lock.json # When we have access to npm 5.5.7, we can replace npm install with npm ci (https://docs.npmjs.com/cli/ci.html)
+  git status
   git add --all ./optimized_images
   git add image-cache.json
 
