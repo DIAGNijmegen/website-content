2c405c0631e12e295b1b77162dcdbb718ce0cd3c
MeykeHermsen <meykehermsen@hotmail.com>
2019-08-13 11:15:16 +0200

    Update pelicanconf.py

--- website-aiimnijmegen/pelicanconf.py
+++ website-aiimnijmegen/pelicanconf.py
@@ -14,7 +14,7 @@ SITE_REPO = 'website-aiimnijmegen'
 
 # Home page and social settings
 SITELEAD = 'The AIIM Group develops, validates and deploys medical image analysis algorithms. Application areas include breast imaging, and radiotherapy applications.'
-SITE_PICTURE = 'images/missing_picture.png'
+SITE_PICTURE = 'images/missing_picture_social.png'
 HOME_IMAGE = 'images/general/ApplicationsOfDeepLearning.png'
 HOME_IMAGE_CAPTION = 'Automated tumor detection'
 TWITTER_URL = 'https://twitter.com/diagnijmegen?ref_src=twsrc%5Etfw'
