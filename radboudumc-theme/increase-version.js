044519125ad1f4ddafd02eff934620edb87b32d8
Wouter Bulten <wouterbulten@mai-ai.org>
2019-08-14 12:12:00 +0200

    Fixes #103

---
+++ radboudumc-theme/increase-version.js
@@ -0,0 +1,12 @@
+// Simple script that reads the css version number and increases it
+//
+const fs = require('fs');
+
+const filePath = '../radboudumc-template/templates/blocks/css.version';
+
+const newVersion = parseInt(fs.readFileSync(filePath));
+
+fs.writeFile(filePath, newVersion + 1, function (err) {
+  if (err) throw err;
+  console.log('Updated CSS version number.');
+});
