8676e7f1cde9bbf46b4f81e29a0e59881a1fea88
Wouter Bulten <wouterbulten@mai-ai.org>
2019-08-14 09:44:59 +0200

    Remove hardcoded pathology descriptions #100

--- website-pathology/pelicanconf.py
+++ website-pathology/pelicanconf.py
@@ -34,7 +34,7 @@ NAV_SECTIONS = [
 ]
 
 # What sections to show on homepage (current options that you customizable: ["Projects", "Software"])
-HOME_SECTIONS = ["Highlights", "Projects", "Members"]
+HOME_SECTIONS = ["Highlights", "Calendar", "Projects", "Members"]
 
 PATH = 'content'
 RELATIVE_URLS = False
