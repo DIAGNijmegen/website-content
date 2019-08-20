d5101439c494b3bf55f0aa704387b4af1c199a98
Wouter Bulten <wouterbulten@mai-ai.org>
2019-08-20 10:18:51 +0200

    Add support for youtube tag #104

--- plugins/inline_extend/inline.py
+++ plugins/inline_extend/inline.py
@@ -20,6 +20,9 @@ group_websites = {
 # group is optional
 regex_member = re.compile(r"\[member:\s*(?P<member>[a-zA-Z\s]+)\s*(,\s*group: (?P<group>[a-zA-Z]+))?\]")
 
+# Matches: [youtube: video_id]
+regex_youtube = re.compile(r"\[youtube:\s*(?P<video>[a-zA-Z0-9\-\_]+)\]")
+
 def parse_member_tag(text):
     """Replaces [member: <name>] tags"""
     name = text.group('member')
@@ -30,6 +33,12 @@ def parse_member_tag(text):
         return f'<a href="{group_websites[group]}/members/{name_url}">{name}</a>'
     else:
         return f'<a href="/members/{name_url}">{name}</a>'
+
+def parse_youtube_tag(text):
+    """Replaces [youtube: id] tags"""
+    video_id = text.group('video')
+
+    return f'<div class="video-container"><iframe src="https://www.youtube-nocookie.com/embed/{video_id}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div>'
 
 def get_settings(generator):
     print(generator.settings['SITEURL'])
@@ -42,6 +51,9 @@ def parse_tags(instance):
 
         if '[member:' in content:
             content = regex_member.sub(lambda m: parse_member_tag(m), content)
+
+        if '[youtube:' in content:
+            content = regex_youtube.sub(lambda m: parse_youtube_tag(m), content)
 
         instance._content = content
 def register():
