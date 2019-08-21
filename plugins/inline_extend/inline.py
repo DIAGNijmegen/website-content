42fe59b509774a26f842f35b3b80330f935dad96
Wouter Bulten <wouterbulten@mai-ai.org>
2019-08-21 10:05:35 +0200

    Add support for vimeo #104

--- plugins/inline_extend/inline.py
+++ plugins/inline_extend/inline.py
@@ -22,6 +22,9 @@ regex_member = re.compile(r"\[member:\s*(?P<member>[a-zA-Z\s]+)\s*(,\s*group: (?
 
 # Matches: [youtube: video_id]
 regex_youtube = re.compile(r"\[youtube:\s*(?P<video>[a-zA-Z0-9\-\_]+)\]")
+# Matches: [vimeo: video_id]
+regex_vimeo = re.compile(r"\[vimeo:\s*(?P<video>[a-zA-Z0-9\-\_]+)\]")
+
 
 def parse_member_tag(text, member_data):
     """Replaces [member: <name>] tags"""
@@ -40,13 +43,17 @@ def parse_member_tag(text, member_data):
         print(f"Member {name} could not be found, no internal link generated.")
         return name
 
-
-
 def parse_youtube_tag(text):
     """Replaces [youtube: id] tags"""
     video_id = text.group('video')
 
     return f'<div class="video-container"><iframe src="https://www.youtube-nocookie.com/embed/{video_id}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div>'
+
+def parse_vimeo_tag(text):
+    """Replaces [vimeo: id] tags"""
+    video_id = text.group('video')
+
+    return f'<div class="video-container"><iframe src="https://player.vimeo.com/video/{video_id}" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>'
 
 def get_settings(generator):
     print(generator.context['MEMBER_DATA'])
@@ -62,6 +69,8 @@ def parse_tags(instance):
 
         if '[youtube:' in content:
             content = regex_youtube.sub(lambda m: parse_youtube_tag(m), content)
+        if '[vimeo:' in content:
+            content = regex_vimeo.sub(lambda m: parse_vimeo_tag(m), content)
 
         instance._content = content
 def register():
