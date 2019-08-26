87475b29abc604c3dc774b83e5e35fc7405d4ed4
Wouter Bulten <wouterbulten@mai-ai.org>
2019-08-26 14:23:08 +0200

    Update publishconf.py

--- website-ai-for-health/publishconf.py
+++ website-ai-for-health/publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://ai-for-health.nl'
+SITEURL = 'https://www.ai-for-health.nl'
 RELATIVE_URLS = False
 
 FEED_DOMAIN = SITEURL
