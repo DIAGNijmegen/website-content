753272c77ff2acc276b488810fe3bc982e1c4258
Wouter Bulten <wouterbulten@mai-ai.org>
2019-08-27 16:26:16 +0200

    Separate build steps

---
+++ optimize_images.sh
@@ -0,0 +1,31 @@
+# #!/usr/bin/env bash
+
+# Break build on error
+set -e
+
+if [[ $TRAVIS_BRANCH != 'master' ]]; then
+  echo "Not on travis-master build, skip running image optimzer (value: $TRAVIS_BRANCH)"
+else
+  # Optimize the images before building the website`
+  cd imgoptim
+  echo "Starting image optimization script"
+  node optimize.js
+
+  git config --global user.email "webteamdiag@gmail.com"
+  git config --global user.name "DIAGWebTeam"
+
+  # Add changed files
+  git checkout master
+  git add --all ./optimized_images
+  git add image-cache.json
+
+  # Commit optimized images back to the repo
+  gitdiff='git diff-index --quiet HEAD .'
+  if ! $gitdiff; then
+    echo "Files changed, commiting new images."
+    git commit --message "Adding optimized images to repository. [ci skip]" -- .
+    git push "https://${GH_PAGES}@github.com/DIAGNijmegen/website-content.git" "master"
+  else
+    echo "Nothing new to commit, skipping push."
+  fi
+fi
