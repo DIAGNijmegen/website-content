8c50582304c948a3797d0d271a85286c7d7b4baa
Wouter Bulten <wouterbulten@mai-ai.org>
2019-08-28 12:11:08 +0200

    Update envs

--- generate_publications.sh
+++ generate_publications.sh
@@ -3,9 +3,7 @@
 # Break build on error, prevents websites going offline in case of pelican errors
 set -e
 
-echo $GENERATE_PUB
-
-if [[ $GENERATE_PUB != 1 ]]; then
+if [[ $GENERATE_PUB != '1' ]]; then
   echo "Skipping generation of publication pages for $WEBSITE."
 else
   echo "Generating publications for $WEBSITE"
