3879554e69fc05b888ab8c8a1f47ba025c296dab
Wouter Bulten <wouterbulten@mai-ai.org>
2019-08-29 09:54:09 +0200

    Clean up build steps

--- deploy.sh
+++ deploy.sh
@@ -6,14 +6,6 @@ set -e
 echo "Starting Pelican build of $WEBSITE"
 cd $WEBSITE
 
-# if [[ $WEBSITE == 'website-pathology' ]] || [[ $WEBSITE == 'website-diag' ]] || [[ $WEBSITE == 'website-neuro' ]]; then
-#   # Init repo
-#   echo "Cloning ${website} output repository"
-#   git clone --depth 1 "https://${GH_PAGES}@github.com/DIAGNijmegen/${website}.git" output
-# else
-#   echo "Website ($WEBSITE) not in deploy pilot, using clean directory."
-# fi
-
 # Build pelican website
 pelican content -s publishconf.py
 
@@ -27,28 +19,6 @@ cp .nojekyll output/.nojekyll
 if [[ -d output/calendar ]]; then
   (cd output/calendar && ls | grep -v index.html | xargs rm -rf)
 fi
-
-# Push to github
-# if [[ $WEBSITE == 'website-pathology' ]] || [[ $WEBSITE == 'website-diag' ]] || [[ $WEBSITE == 'website-neuro' ]]; then
-#   cp CNAME output/CNAME
-#
-#   cd output
-#   git add .
-#   git status
-#
-#   gitdiff='git diff-index --quiet HEAD .'
-#   if ! $gitdiff; then
-#     echo "Files changed, commiting new images."
-#     git commit --message "Pushing new version of ${website}" -- .
-#     git push "https://${GH_PAGES}@github.com/DIAGNijmegen/${website}.git" "master"
-#   else
-#     echo "Nothing new to commit, skipping push."
-#   fi
-#
-#   cd ..
-# else
-#   echo "Website ($WEBSITE) not in deploy pilot, using clean directory."
-# fi
 
 # Go back to root directory
 cd ..
--- optimize_images.sh
+++ optimize_images.sh
@@ -2,30 +2,29 @@
 
 # Break build on error
 set -e
-#
-# if [[ $TRAVIS_BRANCH != 'master' ]]; then
-#   echo "Not on travis-master build, skip running image optimzer (value: $TRAVIS_BRANCH)"
-# else
-  # Optimize the images before building the website`
-  cd imgoptim
-  echo "Starting image optimization script"
-  node optimize.js
 
-  git config --global user.email "webteamdiag@gmail.com"
-  git config --global user.name "DIAGWebTeam"
+# Optimize the images before building the website`
+cd imgoptim
+echo "Starting image optimization script"
+node optimize.js
 
-  # Add changed files
-  git checkout feature/build-optim
-  git add --all ./optimized_images
-  git add image-cache.json
+git config --global user.email "webteamdiag@gmail.com"
+git config --global user.name "DIAGWebTeam"
 
-  # Commit optimized images back to the repo
-  gitdiff='git diff-index --quiet HEAD .'
-  if ! $gitdiff; then
-    echo "Files changed, commiting new images."
-    git commit --message "Adding optimized images to repository. [ci skip]" -- .
-    git push "https://${GH_PAGES}@github.com/DIAGNijmegen/website-content.git" "feature/build-optim"
-  else
-    echo "Nothing new to commit, skipping push."
-  fi
-# fi
+# Add changed files
+git checkout feature/build-optim
+git add --all ./optimized_images
+git add image-cache.json
+
+# Commit optimized images back to the repo
+gitdiff='git diff-index --quiet HEAD .'
+if ! $gitdiff; then
+  echo "Files changed, commiting new images."
+  git commit --message "Adding optimized images to repository. [ci skip]" -- .
+  git push "https://${GH_PAGES}@github.com/DIAGNijmegen/website-content.git" "feature/build-optim"
+else
+  echo "Nothing new to commit, skipping push."
+fi
+
+# Go back to starting dir
+cd ..
