e1a7a0caf654e3a23e62798f24741049d374b577
Wouter Bulten <wouterbulten@mai-ai.org>
2019-08-29 10:05:15 +0200

    Always keep image folders in sync

--- copy_content.sh
+++ copy_content.sh
@@ -8,5 +8,10 @@ python parse_content.py $WEBSITE
 
 # Copy default base pages
 cp -r --no-clobber content/pages/defaults/. $WEBSITE/content/pages/
+
 # Copy images
 cp -r --no-clobber imgoptim/optimized_images/. $WEBSITE/content/images
+
+# Copy non-optimized images (non-overwrite)
+# This makes sure that content is always available, even if it is not processed by the optimizer.
+cp -r --no-clobber content/images/. $WEBSITE/content/images
