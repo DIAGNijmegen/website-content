f38caaa78aa3403053ea2953cc154bb1f019bc4e
martvanrijthoven <mart.vanrijthoven@gmail.com>
2019-09-19 14:28:06 +0200

    another test

--- generate_publications.sh
+++ generate_publications.sh
@@ -23,17 +23,6 @@ else
     cd $WEBSITE
     python plugins/bib_writer.py
     
-    git config --global user.email "webteamdiag@gmail.com"
-    git config --global user.name "DIAGWebTeam"
-
-    # Add changed files
-    git checkout feature/publications 
-    git add --all ./content/pages/publications
-
-    # Commit optimized images back to the repo
-    echo "Publications changed, commiting publications."
-    git commit --message "Adding publications to repository. [ci skip]" -- .
-    git push "https://${GH_PAGES}@github.com/DIAGNijmegen/website-content.git" "feature/publications"
   else
    echo "Diag bib not changed. Skipping generation of publiction (using current publications files)"
   fi
---
+++ push.sh
@@ -0,0 +1,22 @@
+# #!/usr/bin/env bash
+
+# Break build on error, prevents websites going offline in case of pelican errors
+set -e
+
+# Optimize the images before building the website`
+git config --global user.email "webteamdiag@gmail.com"
+git config --global user.name "DIAGWebTeam"
+
+# Add changed files
+git checkout feature/publications
+git add --all ./website-*/content/pages/publications/
+
+gitdiff='git diff --quiet HEAD^ ./content/diag.bib'
+if ! $gitdiff; then
+  echo "Files changed, commiting new publications."
+  git commit --message "Adding publications to repository." -- .
+  git push "https://${GH_PAGES}@github.com/DIAGNijmegen/website-content.git" "feature/publications"
+else
+  echo "Nothing new to commit, skipping push."
+fi
+
