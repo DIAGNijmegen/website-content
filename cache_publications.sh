84e26a8406643ba147215c10b8b7072dc8d6c872
martvanrijthoven <mart.vanrijthoven@gmail.com>
2019-10-01 16:11:19 +0200

    test publications push

--- cache_publications.sh
+++ cache_publications.sh
@@ -43,3 +43,23 @@ do
       fi
     fi
 done
+
+# Optimize the images before building the website`
+git config --global user.email "webteamdiag@gmail.com"
+git config --global user.name "DIAGWebTeam"
+
+# Add changed files
+git checkout feature/publications  
+git pull origin feature/publications
+
+gitdiff='git diff --quiet HEAD^ ./content/diag.bib'
+if ! $gitdiff; then
+  git add --all ./website-*/content/pages/publications
+  git add --all ./website-*/content/dict_pubs.json
+  echo "Files changed, commiting new publications."
+  git commit --message "Adding publications to repository." -- .
+  git push "https://${GH_PAGES}@github.com/DIAGNijmegen/website-content.git" "feature/publications"
+else
+  echo "Nothing new to commit, skipping push."
+fi
+
