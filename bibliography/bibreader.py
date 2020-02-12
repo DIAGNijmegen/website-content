a5febbdb7ecd5f49f0161dc75673c490eba9f040
martvanrijthoven <mart.vanrijthoven@gmail.com>
2020-02-12 14:00:38 +0100

    hot fix if bib item has no year

--- bibliography/bibreader.py
+++ bibliography/bibreader.py
@@ -124,6 +124,8 @@ def parse_bibtex_file(filename):
                 else:
                     bib_item['url_type'] = 'URL'
 
+            if 'year' not in bib_item:
+                bib_item['year'] = '0000'
 
             cover_path = bib_key[0].title() + bib_key[1:] + '.png'
             bib_item['cover_exists'] = str(os.path.exists(os.path.join('.', 'content', 'images', 'theses', cover_path)))
