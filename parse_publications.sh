818c017f3b194f1590f8b50aece9eed6804e1199
martvanrijthoven <mart.vanrijthoven@gmail.com>
2020-03-13 14:55:41 +0100

    Update parse_publications.sh

--- parse_publications.sh
+++ parse_publications.sh
@@ -1,6 +1,6 @@
 # #!/usr/bin/env bash
 
-curl -o content/diag.bib -u ${BIB_TOKEN} ${DIAG_BIB_REPO}diag.bib >/dev/null 2>&1
+curl -o content/diag.bib -u ${BIB_TOKEN} ${DIAG_BIB_REPO}diag2020.bib >/dev/null 2>&1
 curl -o content/fullstrings.bib -u ${BIB_TOKEN} ${DIAG_BIB_REPO}fullstrings.bib >/dev/null 2>&1
 
 python ./bibliography/bibparser.py
