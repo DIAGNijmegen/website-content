970f67dd0cd80cf051f96c68f836b93d9aa75143
Wouter Bulten <wouterbulten@mai-ai.org>
2020-04-30 12:42:16 +0200

    Remove old code

--- plugins/flaskapp/download_pdf.py
+++
@@ -1,6 +0,0 @@
-import dropbox
-dbx = dropbox.Dropbox("r4ABh_NvBxAAAAAAAAABpyZioVr_ClDgJu4h-lMYDgfFgPSplsKGhQGFzV6YhY2J")
-result = dbx.files_get_temporary_link("/Get Started with Dropbox.pdf")
-print('link')
-print(result.link)
-
