99970ff8e3740e690e327eb084609c68742f7d61
Wouter Bulten <wouterbulten@mai-ai.org>
2020-06-10 11:25:15 +0200

    Add srcset for highlights, and add large fallback

--- plugins/imgutil/imgutil.py
+++ plugins/imgutil/imgutil.py
@@ -36,17 +36,21 @@ def srcset_image(path, base_url):
     # Split image path in extension and path (to inject img size)
     parts = os.path.splitext(path)
     
-    for size in SIZE_TO_WIDTH_MAPPING.values():
+    for name, size in SIZE_TO_WIDTH_MAPPING.items():
         # Don't make the full size part of the srcset to save bandwidth
-        if size == 'full':
+        if name == 'full':
             continue
 
         resized_path  = f"{parts[0]}-{size}{parts[1]}"
         
         if os.path.isfile(os.path.join(optim_path, resized_path.replace('images/', ''))):
             srcset.append(f'{base_url}/{resized_path} {size}w')
+        elif name == 'large':
+            # If there is no 'large' version, we use the 'full' version of the image instead for this size.
+            # This makes sure the browser will load the highest resolution iamge for large viewports.
+            # Ideally we would inject the correct width here, 768 is a best-effor guess.
+            srcset.append(f'{base_url}/{resized_path} 768w')
 
-    print(srcset)
     return ', '.join(srcset)
 
 
