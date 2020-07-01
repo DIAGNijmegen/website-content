3b8e08f2458a1fb901e5b9cb3d07ebed01388085
Wouter Bulten <wouterbulten@mai-ai.org>
2020-07-01 10:11:41 +0200

    Rewrite of people page with new sorting method based on lastname

--- plugins/fileutil/fileutil.py
+++ plugins/fileutil/fileutil.py
@@ -13,9 +13,16 @@ def content_exists_or_default(paths, default):
 
     return default
 
+def sort_by_last(pages):
+    """Simple filter to sort by last part of a slug instead of first char"""
+
+    # Pages can be a generator if filters are used (like selectattr)
+    return sorted(list(pages), key=lambda p: p.slug.split('-')[-1])
+
 def add_filter(pelican):
     """Add filters."""
     pelican.env.filters.update({'content_exists_or_default': content_exists_or_default})
+    pelican.env.filters.update({'sort_slug_by_last': sort_by_last})
 
 def register():
     """Plugin registration."""
