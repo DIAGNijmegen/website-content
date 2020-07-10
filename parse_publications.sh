57b245733ef2901c70e0c58ce09ea1fad6cfc1cd
Wouter Bulten <wouterbulten@mai-ai.org>
2020-07-10 11:54:55 +0200

    Add correct output dir for curl requests #217

--- parse_publications.sh
+++ parse_publications.sh
@@ -1,12 +1,12 @@
 # #!/usr/bin/env bash
 
 curl -H 'Authorization: token ${GH_BIB_TOKEN}' \
-    -O \
+    -o content/diag.bib \
     -s \
     -L https://raw.githubusercontent.com/DIAGNijmegen/diag-literature/master/diag.bib
 
 curl -H 'Authorization: token ${GH_BIB_TOKEN}' \
-    -O \
+    -o content/fullstrings.bib \
     -s \
     -L https://raw.githubusercontent.com/DIAGNijmegen/diag-literature/master/fullstrings.bib
 
