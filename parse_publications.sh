6fc941dee5c45f05096750fa3f1754da6a7da781
Wouter Bulten <wouterbulten@mai-ai.org>
2020-07-10 12:09:59 +0200

    Update parse_publications.sh

--- parse_publications.sh
+++ parse_publications.sh
@@ -2,10 +2,14 @@
 
 curl -H 'Authorization: token ${GH_BIB_TOKEN}' \
     -o content/diag.bib \
+    -S \
+    -D - \
     -L https://raw.githubusercontent.com/DIAGNijmegen/diag-literature/master/diag.bib
 
 curl -H 'Authorization: token ${GH_BIB_TOKEN}' \
     -o content/fullstrings.bib \
+    -S \
+    -D - \
     -L https://raw.githubusercontent.com/DIAGNijmegen/diag-literature/master/fullstrings.bib
 
 python ./bibliography/bibparser.py
