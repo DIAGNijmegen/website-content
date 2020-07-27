b297672afabe085b916b5568fb5f94ced963993f
Wouter Bulten <wouterbulten@mai-ai.org>
2020-07-27 16:39:49 +0200

    All images are in cdn so they don't have to be copied anymore

--- copy_content.sh
+++ copy_content.sh
@@ -9,20 +9,6 @@ python parse_content.py $WEBSITE
 # Copy default base pages
 cp -r --no-clobber content/pages/defaults/. $WEBSITE/content/pages/
 
-# Copy images
-if [[ -z "$SKIP_IMG_COPY" ]]; then
-    echo "Start copying images"
-    
-    cp -r --no-clobber assets/images/. $WEBSITE/content/images
-
-    # Copy non-optimized images (non-overwrite)
-    # This makes sure that content is always available, even if it is not processed by the optimizer.
-    # TODO: This can be removed if all websites use the CDN.
-    cp -r --no-clobber content/images/. $WEBSITE/content/images
-else
-    echo "Skipping copying images for this build."
-fi
-
 # copy bib files
 cp content/bibitems.json $WEBSITE/content/bibitems.json
 cp content/authorkeys.json $WEBSITE/content/authorkeys.json
