0a9ab34ad1de13e4be1b1149992e9f0ae5417a5e
Wouter Bulten <wouterbulten@mai-ai.org>
2020-08-25 15:25:29 +0200

    Enable edit urls for research lines

--- plugins/edit_url/pelican_edit_url.py
+++ plugins/edit_url/pelican_edit_url.py
@@ -21,7 +21,7 @@ def add_edit_url(instance):
     # assume the parent of content dir is project dir
     PATH = os.path.dirname(instance.settings['PATH'])
 
-    shared_dirs = ['highlights', 'members', 'projects', 'presentations', 'software', 'vacancies']
+    shared_dirs = ['highlights', 'members', 'projects', 'presentations', 'software', 'vacancies', 'research']
     generated_pages = ['presentations.md', 'members.md', 'projects.md', 'software.md', 'publications.md', 'thesis-gallery.md', 'vacancies.md']
     default_pages = ['colofon.md', '404.md', 'home.md']
 
