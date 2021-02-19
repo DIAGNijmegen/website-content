3c30e928c5c6e4d222221457588fd462f9b9839f
Rob Tolboom <robtolboom@gmail.com>
2021-02-19 15:51:24 +0100

    site

--- website-anes/pelicanconf.py
+++ website-anes/pelicanconf.py
@@ -7,32 +7,32 @@ from datetime import date
 # Site specific variables
 # Please update these to customize the website.
 #
-AUTHOR = "DIAG RSE"
-SITENAME = "DIAG Research Software Engineering"
-SITENAME_SHORT = "DIAG RSE"
+AUTHOR = "AI for Anesthesiology"
+SITENAME = "AI for Anesthesiology"
+SITENAME_SHORT = "AI 4 A"
 SITE_REPO = "website-anes"
 SITE_GROUP = "anes"
 
 # Home page and social settings
 SITELEAD = """
-The Research Software Engineering Team is part of the Diagnostic Image Analysis Group at Radboud University Medical Center in Nijmegen, The Netherlands.<br><br>
-We use industry best practices to develop software that enables researchers to rapidly develop novel machine learning algorithms that we can integrate into research workstations for clinical validation.
+Blablabla
 """
 SITE_PICTURE = "images/social/missing_picture_social.png"
 HOME_IMAGE = "images/general/ApplicationsOfDeepLearning.png"
 HOME_IMAGE_CAPTION = ""
-TWITTER_URL = "https://twitter.com/diagnijmegen?ref_src=twsrc%5Etfw"
-FOOTER_TEXT = ""
+#TWITTER_URL = "https://twitter.com/diagnijmegen?ref_src=twsrc%5Etfw"
+FOOTER_TEXT = "The AI for Anesthesiology Group is part of the Department of Anesthesiology, Pain and Palliative Medicine at <a href="https://www.radboudumc.nl">Radboud University Medical Center</a>."
 TOP_DOMAIN = '<a href="https://www.radboudumc.nl">Radboudumc</a>'
-PARENT_DOMAIN = '<a href="http://www.radboudimaging.nl">Radboud Imaging</a>'
+PARENT_DOMAIN = '<a href="https://www.radboudumc.nl/afdelingen/anesthesiologie-pijn-en-palliatieve-geneeskunde">Anesthesiology</a>'
 
 # What sections to show in the nav bar
 NAV_SECTIONS = [
-    # {"name": "Highlights", "icon": "megaphone"},
-    {"name": "People", "url": "members", "icon": "users"},
-    # {"name": "Projects", "url": "projects", "icon": "folder"},
-    {"name": "Software", "url": "software", "icon": "code"},
+    {"name": "News", "url": "news", "icon": "megaphone"},
+    {"name": "Members", "url": "members", "icon": "users"},
+    {"name": "Projects", "url": "projects", "icon": "folder"},
     {"name": "Vacancies", "url": "vacancies", "icon": "vacancies"},
+    {"name": "Publications", "url": f"publications/{CURRENTYEAR}", "icon": "file-text-o"},
+    {"name": "Thesis Gallery", "url": "thesis-gallery", "icon": "book"},
     {"name": "Contact", "url": "contact", "icon": "envelope-o"},
 ]
 
@@ -40,13 +40,18 @@ NAV_SECTIONS = [
 ENABLE_BREADCRUMBS = True
 
 # What sections to show on homepage (current options that you customizable: {section_name: custom_name})
-HOME_SECTIONS = {"Vacancies": "Vacancies", "Software": "Software", "Members": "Members"}
+HOME_SECTIONS = {
+    "News": "News",
+    "Vacancies": "Vacancies",
+    "Projects": "Projects",
+    "Members": "Members",
+}
 
 # URLs
 SITEURL = ""
 IMGURL = SITEURL
 EDIT_CONTENT_URL = (
-    "https://github.com/diagnijmegen/website-content/edit/master/{file_path}"
+    "https://github.com/AIforAnesthesiology/website-content/edit/master/{file_path}"
 )
 
 #
@@ -82,9 +87,9 @@ PAGE_URL = "{slug}/"
 PAGE_SAVE_AS = "{slug}/index.html"
 SLUGIFY_SOURCE = "basename"
 
-ARTICLE_URL = "highlights/{slug}/"
-ARTICLE_SAVE_AS = "highlights/{slug}/index.html"
-ARTICLE_TYPE = "Highlights"
+ARTICLE_URL = "news/{slug}/"
+ARTICLE_SAVE_AS = "news/{slug}/index.html"
+ARTICLE_TYPE = "News"
 
 TAGS_SAVE_AS = ""
 TAG_SAVE_AS = ""
@@ -95,7 +100,7 @@ CATEGORIES_SAVE_AS = ""
 ARCHIVES_SAVE_AS = ""
 
 SITEMAP_SAVE_AS = "sitemap.xml"
-INDEX_SAVE_AS = "highlights/index.html"
+INDEX_SAVE_AS = "news/index.html"
 
 # Theme settings
 THEME = "../radboudumc-template"
