145da328475e930cebfbd086a2de0cb3c399d6e1
Nikolas Lessmann <773597+nlessmann@users.noreply.github.com>
2022-03-17 12:54:26 +0100

    Copy only those default pages that are actually used (#438)

--- parse_content.py
+++ parse_content.py
@@ -3,7 +3,19 @@ import glob
 import shutil
 import sys
 
-directories = ['members', 'news', 'presentations', 'projects', 'software', 'vacancies', 'calendar', 'publications', 'research']
+from pelican.settings import get_settings_from_file
+
+directories = [
+    "members",
+    "news",
+    "presentations",
+    "projects",
+    "software",
+    "vacancies",
+    "calendar",
+    "publications",
+    "research",
+]
 
 site = sys.argv[1]
 group_name = site[8:]
@@ -16,39 +28,41 @@ for dir in directories:
         os.mkdir(output_dir)
 
 for dir in directories:
-
-    files = glob.glob(os.path.join('content', 'pages', dir, '*.md'))
+    files = glob.glob(os.path.join("content", "pages", dir, "*.md"))
 
     for file_path in files:
         filename = os.path.basename(file_path)
         with open(file_path) as file:
             try:
                 for line in file:
-                    if 'groups:' in line:
+                    if "groups:" in line:
+                        groups = line.split(":")[1].replace(" ", "").rstrip().split(",")
 
-                            groups = line.split(':')[1].replace(' ','').rstrip().split(',')
+                        # Check if the content belongs to the current website
+                        if group_name in groups:
+                            if dir == "news":
+                                # Write hightlights to directory out of pages dir
+                                out_dir = os.path.join(site, "content", dir)
+                            else:
+                                out_dir = os.path.join(site, "content", "pages", dir)
 
-                            # Check if the content belongs to the current website
-                            if group_name in groups:
-                                if dir == 'news':
-                                    # Write hightlights to directory out of pages dir
-                                    out_dir = os.path.join(site, 'content', dir)
-                                else:
-                                    out_dir = os.path.join(site, 'content', 'pages', dir)
+                            if not os.path.exists(out_dir):
+                                os.makedirs(out_dir)
+                            shutil.copyfile(file_path, os.path.join(out_dir, filename))
 
-                                if not os.path.exists(out_dir):
-                                    os.makedirs(out_dir)
-                                shutil.copyfile(file_path, os.path.join(out_dir, filename))
-  
-
-
-                            # Stop parsing file
-                            break
+                        # Stop parsing file
+                        break
             except Exception as e:
                 print(f"Error parsing {file_path}.")
                 print(e)
 
-        
+# Copy only the default pages that are actually used
+settings = get_settings_from_file(os.path.join(site, "pelicanconf.py"))
+for section in settings["NAV_SECTIONS"]:
+    page = section["url"].split("/")[0] + ".md"
+    src_path = os.path.join("content", "pages", "defaults", page)
+    dst_path = os.path.join(site, "content", "pages", page)
+    if os.path.exists(src_path) and not os.path.exists(dst_path):
+        shutil.copyfile(src_path, dst_path)
 
-
-print(f'Copied pages to {site}.')
+print(f"Copied pages to {site}.")
