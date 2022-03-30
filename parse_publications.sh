1239c497895f22a890d109fda4ecc62f1cb7e548
nlessmann <773597+nlessmann@users.noreply.github.com>
2022-03-30 16:51:40 +0200

    Make curl crash when authentication fails

--- parse_publications.sh
+++ parse_publications.sh
@@ -4,12 +4,12 @@ set -e
 
 curl -H "Authorization: token ${GH_BIB_TOKEN}" \
     -o content/diag.bib \
-    -s \
+    -s -f -S \
     -L https://raw.githubusercontent.com/DIAGNijmegen/diag-literature/master/diag.bib
 
 curl -H "Authorization: token ${GH_BIB_TOKEN}" \
     -o content/fullstrings.bib \
-    -s \
+    -s -f -S \
     -L https://raw.githubusercontent.com/DIAGNijmegen/diag-literature/master/fullstrings.bib
 
 python ./bibliography/bibparser.py
