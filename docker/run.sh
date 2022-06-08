c8e6c88d413bb02203ce2f1845321b9410175417
Jasper Linmans <jasper@Jaspers-MacBook-Pro.local>
2022-06-08 15:04:26 +0200

    auto pull branch from website-content repo

--- docker/run.sh
+++ docker/run.sh
@@ -1,19 +1,36 @@
 #!/bin/bash:
 
-cd /home/user/website-content
-echo "Going to initialize website: ${@}"
-echo 
-
-# Copy bib files and parse, if output files do not exist.
-if [ ! -f /home/user/website-content/content/bibitems.json ]; then
-    cp docker/bibfiles/diag.bib content/diag.bib
-    cp docker/bibfiles/fullstrings.bib content/fullstrings.bib
-    python ./bibliography/bibparser.py
+if [ $# -eq 2 ]; then
+    echo "Going to clone website-content repository..."
+    rm -r /home/user/website-content-repository-clone
+    git clone https://github.com/DIAGNijmegen/website-content.git /home/user/website-content-repository-clone
+    echo "Done cloning."
+    echo "Going to checkout at branch: $2"
+    cd /home/user/website-content-repository-clone
+    git fetch
+    git checkout $2
+elif [ $# -eq 1 ]; then
+    cd /home/user/website-content
+    echo "Going to initialize website from local volume: ${@}"
+    echo 
+else
+    echo "Incorrect number of command line arguments provided"
+    exit 0
 fi
+
+echo "Going to clone Literature repository..."
+rm -r docker/literature_repository
+git clone https://github.com/DIAGNijmegen/Literature.git docker/literature_repository
+echo "Done cloning."
+cp docker/literature_repository/diag.bib content/diag.bib
+cp docker/literature_repository/fullstrings.bib content/fullstrings.bib
+rm -r docker/literature_repository
+python ./bibliography/bibparser.py
+echo "Done with bibparser.py" 
 
 # Execute copy content script.
 LOCAL=1 WEBSITE=website-"${@}" sh ./copy_content.sh
 
 # Run pelican.
-cd /home/user/website-content/website-"${@}"
+cd website-"${@}"
 pelican --autoreload --listen --bind 0.0.0.0
