8e4ad54a291fe5909650822a07062c47abed5702
Marina D'Amato <marinadamato.md@gmail.com>
2023-04-26 13:48:00 +0200

    Changed grouputil plugin

--- plugins/grouputil/grouputil.py
+++ plugins/grouputil/grouputil.py
@@ -3,6 +3,29 @@ from copy import deepcopy
 from pelican import signals
 
 
+def update_members(members, group_name):
+    new_members = []
+    
+    for person in members:
+        updated_person = deepcopy(person)
+        groups = updated_person.groups
+        groups = groups.replace(" ", "").split(',')
+        indx = groups.index(group_name)
+        fields = [updated_person.active, updated_person.type, updated_person.position]
+        new_values = []
+        for values in fields:
+            values = values.split(',') if isinstance(values, str) else values
+            if len(values) > 1:
+                new_value = values[indx].strip()
+            else:
+                new_value = values[0]
+            new_values.append(new_value)
+        updated_person.active = new_values[0]
+        updated_person.type = new_values[1]
+        updated_person.position = new_values[2]
+        new_members.append(updated_person)
+    return new_members
+
 def update_person(person, group_name):
     updated_person = deepcopy(person)
     groups = updated_person.groups
@@ -25,6 +48,7 @@ def update_person(person, group_name):
 
 def add_filter(pelican):
     """Add filters."""
+    pelican.env.filters.update({'update_members': update_members})
     pelican.env.filters.update({'update_person': update_person})
 
 def register():
