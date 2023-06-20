571c58900bd3e9b99e83641b311c104fa97d720e
Dre Peeters <103488303+drepeeters@users.noreply.github.com>
2023-06-20 09:16:19 +0200

    Update homepage header

--- website-diag/pelicanconf.py
+++ website-diag/pelicanconf.py
@@ -21,7 +21,7 @@ SITE_REPO = "website-diag"
 SITE_GROUP = "diag"
 
 # Home page, layout and social settings
-SITELEAD = "The Diagnostic Image Analysis Group is part of the Departments of Imaging, Pathology, Ophthalmology, and Radiation Oncology of Radboud University Medical Center. We develop computer algorithms to interpret and process medical images."
+SITELEAD = "The Diagnostic Image Analysis Group is part of the Departments of Imaging, Pathology, Radiation Oncology, Cardiology, and Neurology of Radboud University Medical Center. We develop computer algorithms to interpret and process medical images."
 SITE_PICTURE = "images/social/missing_picture_social.png"
 NUM_NEWS_HOME_PAGE = 4
 HOME_IMAGE = "highlight"
