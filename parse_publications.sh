bfab8861f2de6bbb764eb4cd9a010334ff6e46f0
Rob Tolboom <robtolboom@gmail.com>
2024-12-04 15:31:20 +0100

    Added thesis test?

--- parse_publications.sh
+++ parse_publications.sh
@@ -2,17 +2,17 @@
 
 set -e
 
-curl -o content/diag.bib \
-     -s -f -S \
-     -L https://raw.githubusercontent.com/DIAGNijmegen/Literature/main/diag.bib
+#curl -o content/diag.bib \
+#     -s -f -S \
+#     -L https://raw.githubusercontent.com/DIAGNijmegen/Literature/main/diag.bib
 
-curl -o content/cara.bib \
-     -s -f -S \
-     -L https://raw.githubusercontent.com/DIAGNijmegen/Literature/main/cara.bib
+#curl -o content/cara.bib \
+#     -s -f -S \
+#     -L https://raw.githubusercontent.com/DIAGNijmegen/Literature/main/cara.bib
 
-curl -o content/fullstrings.bib \
-     -s -f -S \
-     -L https://raw.githubusercontent.com/DIAGNijmegen/Literature/main/fullstrings.bib
+#curl -o content/fullstrings.bib \
+#     -s -f -S \
+#     -L https://raw.githubusercontent.com/DIAGNijmegen/Literature/main/fullstrings.bib
 
 python ./bibliography/bibparser.py diag
-python ./bibliography/bibparser.py cara
+#python ./bibliography/bibparser.py cara
