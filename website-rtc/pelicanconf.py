64123467dd4e74a006ededbdec4b25fc8382fc48
SilvanQuax <s.c.quax@gmail.com>
2025-10-21 17:26:17 +0200

    Update pelicanconf.py
    add AI4H course page

--- website-rtc/pelicanconf.py
+++ website-rtc/pelicanconf.py
@@ -114,6 +114,7 @@ DIRECT_TEMPLATES = ["index", "archives", "sitemap"]
 ARTICLE_EXCLUDES = ['templates']
 TEMPLATE_PAGES = {
     'templates/homepage.html': 'index.html',
+    'templates/courses.html': 'courses.html',
 }
 
 # Plugins
