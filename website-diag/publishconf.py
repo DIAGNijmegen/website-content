a4526746b30d2ec220e973c3ff8482e23c0b5f2e
drepeeters <dre.peeters@hotmail.com>
2025-12-10 13:28:25 +0100

    Adjust image url

--- website-diag/publishconf.py
+++ website-diag/publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 SITEURL = 'https://spiffy-duckanoo-c7fa75.netlify.app/' #'https://www.diagnijmegen.nl'
-IMGURL =  'https://assets.spiffy-duckanoo-c7fa75.netlify.app/'#'https://assets.diagnijmegen.nl'
+IMGURL =  'https://assets.diagnijmegen.nl'
 RELATIVE_URLS = False
 
 FEED_DOMAIN = SITEURL
